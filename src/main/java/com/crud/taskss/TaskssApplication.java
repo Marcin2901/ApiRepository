269d2191715374d41c1dd183d7078ecc96426d56
Chudy4822 <jamarcin2901@wp.pl>
2020-10-24 12:16:52 +0200

    Moduł 14.3

---
+++ src/main/java/com/crud/taskss/TaskssApplication.java
@@ -0,0 +1,14 @@
+package com.crud.taskss;
+
+import com.crud.taskss.domain.TaskDto;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskssApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/crud/taskss/domain/Task.java
@@ -0,0 +1,7 @@
+package com.crud.taskss.domain;
+
+public class Task {
+    private Long id;
+    private String title;
+    private String content;
+}
